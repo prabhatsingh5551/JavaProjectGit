3354fb76c2cf9b9630bf3edbfa857dd1c81a1d4f
Prabhat Singh <Prabhat Singh@LAPTOP-7DABDMA0>
2022-06-18 11:44:09 +0530

    this is my first commit

---
+++ src/Test1.java
@@ -0,0 +1,16 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		String hungry = "yes";
+		
+		if(hungry == "yes")
+			System.out.println("est something........");
+		else {
+			System.out.println("done something................");
+		}
+		
+	}
+	
+}
